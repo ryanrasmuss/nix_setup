f53494491a3970d63b5d57db40aec8922edfd6fb
ryan <ryan>
2018-01-01 16:32:57 -0800

    basic setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# *Nix Setup
+
+A simple script for my linux machines
+
---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Script for needed tools
+sudo apt-get install vim
+sudo apt-get install ssh
+cat bash_settings.txt >> ~/.bashrc
+cat vim_settings.txt >> ~/.vimrc
