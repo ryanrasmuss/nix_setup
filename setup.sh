f0f8f080d0872e85ddbc617fa1e77cae37010035
ryan <ry@naw.com>
2018-01-14 20:57:13 -0800

    new cursorline

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # Script for needed tools
 sudo apt-get install vim
 sudo apt-get install ssh
+# sudo apt-get install openssh-server
 cat bash_settings.txt >> ~/.bashrc
 cat vim_settings.txt >> ~/.vimrc
