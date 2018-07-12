de4be4824b395fff85b1d0685f087e1e55a6d640
ryan <ryanrasmuss@users.noreply.github.com>
2018-07-12 15:48:07 -0700

    added new aliases

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Script for needed tools
-sudo apt-get install vim
-sudo apt-get install ssh
-sudo apt-get install openssh-server
+sudo apt-get install vim -y
+sudo apt-get install ssh -y
+sudo apt-get install openssh-server -y
 cat bash_settings.txt >> ~/.bash_aliases
 cat vim_settings.txt >> ~/.vimrc
 source ~/.bashrc
