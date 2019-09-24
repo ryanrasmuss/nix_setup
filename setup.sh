193b23c8b1710ec5c7f8f6137b3255df78bfd7b8
ryan <ryanrasmuss@users.noreply.github.com>
2019-09-24 12:08:33 -0700

    colors for vim

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@
 #sudo apt-get install git -y
 cat bash_settings.txt >> ~/.bash_aliases
 cat vim_settings.txt >> ~/.vimrc
+mkdir -p ~/.vim/colors/
+curl -vO https://github.com/tomasr/molokai/blob/master/colors/molokai.vim
+curl -vO https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+mv -v *.vim ~/.vim/colors/
