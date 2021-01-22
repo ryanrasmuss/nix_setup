8d47102449f297b3526ccf3b42817897418fce66
Ryan Rasmuss <ryanrasmuss@users.noreply.github.com>
2021-01-22 11:45:13 -0800

    new bash prompt

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Script for needed tools
-#sudo apt-get install vim -y
+sudo apt-get install vim -y
 #sudo apt-get install ssh -y
 #sudo apt-get install openssh-server -y
 #sudo apt-get install git -y
